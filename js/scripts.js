4c47a07b012dc08a818bf15fd0272507ffc6dd6f
nic netzel and yvonna contreras <netzeln@gmail.com>
2016-01-25 14:49:05 -0800

    Add functionality to Second Results list

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,21 @@ $("form#placeCollector").submit(function(event){
   event.preventDefault();
   var placeName = $("input#placeName").val();
   var placeLocation = $("input#placeLocation").val();
-  var placeSeason = $("input#placeName").val();
-  var placeNote = $("input#placeNote").val();
-  var newPlace = new Place{placeName, placeLocation, placeSeason, placeNote};
-
+  var placeSeason = $("input#placeSeason").val();
+  var placeNotes = $("input#placeNotes").val();
+  var newPlace = new Place(placeName, placeLocation, placeSeason, placeNotes);
+console.log(newPlace);
   $("ul#listResults").append("<li><span class='clickable'>" + newPlace.name + "</span></li>");
-})
+  $("#showResults").show();
+
+$(".clickable").last().click(function(){
+  $("#showResults2").show();
+  $("#showResults2 h3").text(newPlace.name);
+  $(".viewLocation").text(newPlace.location);
+  $(".viewSeason").text(newPlace.season);
+  $(".viewNotes").text(newPlace.notes);
+});
+});
 
 
 });
